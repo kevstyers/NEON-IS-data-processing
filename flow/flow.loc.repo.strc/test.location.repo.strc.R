f48e83e4abb639d547301cb89014a1b7c900e7cc
visalakshi chundru <vchundru@battelleecology.org>
2020-02-10 14:45:24 -0700

    test cases for flow.loc.repo.strc

---
+++ flow/flow.loc.repo.strc/test.location.repo.strc.R
@@ -0,0 +1,59 @@
+library(testthat)
+source("location_repo_strc.R")
+test_that(
+   "Threshold Filter test expect 2 thresholds in the output file",
+   {
+     DirIn <- "tests/test_input/pfs/threshold"
+     DirOut <- "tests/test_output/pfs/out"
+     Term <- "temp"
+     Ctxt <- "aspirated-single"
+     OutPutFile <- file.path(DirOut, "thresholds.json")
+     filter_threshold(DirIn,DirOut, Term,Ctxt)
+     result <- rjson::fromJSON(file=OutPutFile,simplify=TRUE)
+     expect_true(length(result$thresholds) ==2)
+
+   }
+ )
+
+
+test_that(
+  "Threshold Filter test expect zero thresholds in the input file",
+  {
+    DirIn <- "tests/test_input/pfs/threshold_fail"
+    DirOut <- "tests/test_output/pfs/threshold_fail"
+    Term <- "temp"
+    Ctxt <- "aspirated-single"
+    OutPutFile <- file.path(DirOut, "thresholds.json")
+    filter_threshold(DirIn,DirOut, Term,Ctxt)
+    result <- rjson::fromJSON(file = OutPutFile,simplify=TRUE)
+    expect_true(length(result$thresholds) ==0)
+  }
+)
+
+test_that(
+  "Threshold Filter test expect a thresholds with only Term and no Context in the input file",
+  {
+    DirIn <- "tests/test_input/pfs/threshold_with_only_term"
+    DirOut <- "tests/test_output/pfs/threshold_with_only_term"
+    Term <- "dewPoint"
+    Ctxt <- "aspirated-single"
+    OutPutFile <- file.path(DirOut, "thresholds.json")
+    filter_threshold(DirIn, DirOut, Term=Term)
+    result <- rjson::fromJSON(file = OutPutFile,simplify=TRUE)
+    expect_true(length(result$thresholds) ==3)
+  }
+)
+
+test_that(
+  "Threshold Filter test expect a thresholds with only context and no term in the input file",
+  {
+    DirIn <- "tests/test_input/pfs/threshold_with_only_context"
+    DirOut <- "tests/test_output/pfs/threshold_with_only_context"
+  # Term <- "temp"
+    Ctxt <- "aspirated-single"
+    OutPutFile <- file.path(DirOut, "thresholds.json")
+    filter_threshold(DirIn,DirOut,Ctxt = Ctxt)
+    result <- rjson::fromJSON(file = OutPutFile,simplify=TRUE)
+    expect_true(length(result$thresholds) ==2)
+  }
+)
